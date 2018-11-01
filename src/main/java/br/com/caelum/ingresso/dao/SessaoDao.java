af15c88376d60a824d5da17b88110737710d923e
G.Stocco <piresstocco.gabriel@gmail.com>
2018-11-01 10:55:30 -0200

    Alteraçoes para adicionar compra e lugares

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -7,12 +7,17 @@ import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
 
+import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 
 @Repository
 public class SessaoDao {
 
+	public Sessao findOne(Integer id){
+		return manager.find(Sessao.class, id);
+	}
+	
 	@PersistenceContext
 	private EntityManager manager;
 	
@@ -22,5 +27,10 @@ public class SessaoDao {
 	
 	public List<Sessao> buscaSessoesDaSala(Sala sala){
 		return manager.createQuery("select s from Sessao s where s.sala = :sala", Sessao.class).setParameter("sala", sala).getResultList();
+	
+	}
+		public List<Sessao> buscaSessoesDoFilme(Filme filme){
+			return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class).setParameter("filme", filme).getResultList();
+	
 	}
 }
